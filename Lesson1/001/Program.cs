23192680ca9903bd0e77887a46e2867c167ea6d4
Alena Zvereva <angryvex@gmail.com>
2022-08-19 23:10:58 +0300

    add the 1st homework problem

---
+++ Lesson1/001/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Пожалуйста, введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine(a + " больше, чем " + b);
+}
+else if (a == b)
+{
+    Console.WriteLine("Введенные числа равны");
+}
+else
+{
+    Console.WriteLine(b + " больше, чем " + a);
+}
+
