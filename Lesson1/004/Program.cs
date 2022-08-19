0f616e89cc804c75464ffe7a39353b8901bfbca3
Alena Zvereva <angryvex@gmail.com>
2022-08-19 23:32:46 +0300

    add the 4th homework problem

---
+++ Lesson1/004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Пожалуйста, введите число: ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
