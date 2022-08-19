bb00d1d3480f766d4d65eb25b6e7122584b38d53
Alena Zvereva <angryvex@gmail.com>
2022-08-19 23:19:25 +0300

    add the 2nd homework problem

---
+++ Lesson1/002/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Пожалуйста, введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Пожалуйста, введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b & a > c) 
+{
+    Console.WriteLine(a + " самое большое число из введенных");
+} 
+else if (b > a & b > c)
+{
+    Console.WriteLine(b + " самое большое число из введенных");
+}
+else
+{
+    Console.WriteLine(c + " самое большое число из введенных");
+}
