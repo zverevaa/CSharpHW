c4669a3ae41f732cb991ae78ff8dc60397ee6447
Alena Zvereva <angryvex@gmail.com>
2022-08-19 23:25:56 +0300

    add the 3rd homework problem

---
+++ Lesson1/003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Пожалуйста, введите число для проверки на четность: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число " + a + " является четным.");
+}
+else
+{
+    Console.WriteLine("Число " + a + " не является четным.");
+}
