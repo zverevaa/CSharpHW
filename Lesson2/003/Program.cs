7183faadb66744d98fd4b9f0f2b664fa73149f37
Alena Zvereva <angryvex@gmail.com>
2022-08-22 22:27:24 +0300

    add 3rd problem

---
+++ Lesson2/003/Program.cs
@@ -0,0 +1,12 @@
+﻿int weekday = new Random().Next(1, 8);
+
+if (weekday == 6 || weekday == 7)
+{
+    Console.WriteLine("Сегодня выходной");
+}
+else
+{
+    Console.WriteLine("Сегодня будний день");
+}
+
+Console.WriteLine(weekday);
