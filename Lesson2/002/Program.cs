ed25c15010d39ef60de1c51c10d273275b4adb24
Alena Zvereva <angryvex@gmail.com>
2022-08-22 22:21:43 +0300

    add the 2nd problem

---
+++ Lesson2/002/Program.cs
@@ -0,0 +1,12 @@
+﻿int num = new Random().Next(1, 150);
+
+int num1 = num % 10;
+
+if (num / 100 == 0)
+{
+    Console.WriteLine($"В числе {num} нет третьей цифры");
+}
+else
+{
+    Console.WriteLine($"Третья цифра числа {num} - {num1}");
+}
