0be749a00e55e640f5ee04475c88486fd3bcc95f
Alena Zvereva <angryvex@gmail.com>
2022-08-27 20:42:14 +0300

    Add Lesson 3 problem 001

---
+++ Lesson3/001/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите пятизначное число: ");
+string num = Console.ReadLine();
+int isNumericHelper = 0;
+bool isNumeric = int.TryParse(num, out isNumericHelper);
+
+
+if(num.Length == 5 && isNumeric)
+{
+    if(num[0] == num[4] && num[1] == num[3]) 
+    {
+        Console.WriteLine($"Число {num} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num} не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Введенные Вами данные не являются числом, или число не является пятизначным. Попробуйте еще раз");
+}
