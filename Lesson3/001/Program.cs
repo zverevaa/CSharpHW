24522a854f2822c2feb6dbd2a7afed1547c0e697
Alena Zvereva <angryvex@gmail.com>
2022-08-27 21:21:23 +0300

    Cleanup before pushing

--- Lesson3/001/Program.cs
+++ Lesson3/001/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.Write("Введите пятизначное число: ");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
 string num = Console.ReadLine();
 int isNumericHelper = 0;
 bool isNumeric = int.TryParse(num, out isNumericHelper);
