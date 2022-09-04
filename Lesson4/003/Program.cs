1db034022f27510254f1e55c0216dea6fb2b2465
Alena Zvereva <angryvex@gmail.com>
2022-09-04 11:45:35 +0300

    add lesson 4 problem 003

---
+++ Lesson4/003/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] nums = new int[8];
+
+void FillArray(int[] collection)
+{
+    for(int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.Write($"{array[i]}");
+        }
+        else
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.WriteLine();
+}
+
+FillArray(nums);
+PrintArray(nums);
