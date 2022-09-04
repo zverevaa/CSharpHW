79e38e38a27156562270309c409629d6c26a5c3d
Alena Zvereva <angryvex@gmail.com>
2022-09-04 11:22:19 +0300

    add lesson 4 problem 001

---
+++ Lesson4/001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int GetExponent(int num, int exponent)
+{
+    int result = 1;
+    for(int i = 1; i <= exponent; i++)
+    {
+        result *= num;
+    }
+    return result;
+}
