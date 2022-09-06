e6453599765d0b9d79e2b13fea965693a3b805fd
Alena Zvereva <angryvex@gmail.com>
2022-09-07 00:23:38 +0300

    add Lesson 5 problem 002 and 003

---
+++ Lesson5/002/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] nums = new int[10];
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+        Console.Write(array[i] + " ");
+    }
+}
+
+int AddUneven(int[] array)
+{
+    int sum = 0;
+    int i = 1;
+    while(i < array.Length)
+    {
+        sum += array[i];
+        i += 2;
+    }
+
+    return sum;
+}
+FillArray(nums);
+Console.WriteLine(AddUneven(nums));
+
---
+++ Lesson5/003/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int[] nums = new int[10];
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        Console.Write(array[i] + " ");
+    }
+}
+
+// int FindMin(int[] array)
+// {
+//     int min = array[0];
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] < min)
+//         {
+//             min = array[i];
+//         }
+//         else
+//         {
+//             continue;
+//         }
+//     }
+//     return min;
+// }
+
+// int FindMax(int[] array)
+// {
+//     int max = array[0];
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] > max)
+//         {
+//             max = array[i];
+//         }
+//         else
+//         {
+//             continue;
+//         }
+//     }
+//     return max;
+// }
+
+int FindDiff(int[] array)
+{
+    int max = array[0];
+    int min = array[0];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < min)
+        {
+            min = array[i];
+        }
+        else if(array[i] > max)
+        {
+            max = array[i];
+        }
+        else
+        {
+            continue;
+        }
+    }
+    Console.WriteLine(max);
+    Console.WriteLine(min);
+    return max - min;
+}
+FillArray(nums);
+Console.WriteLine(FindDiff(nums));
