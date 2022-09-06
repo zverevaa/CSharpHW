8414748c2177eeab434aef605f12916c50a0daac
Alena Zvereva <angryvex@gmail.com>
2022-09-07 00:01:54 +0300

    add Lesson 5 problem 001

---
+++ Lesson5/001/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] nums = new int[10];
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        Console.Write(array[i] + " ");
+    }
+}
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count += 1;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return count;
+}
+
+FillArray(nums);
+Console.WriteLine(CountEven(nums));
