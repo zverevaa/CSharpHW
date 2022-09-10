cfa1133702d76f16ef6f5dd0be7635a3852d187f
Alena Zvereva <angryvex@gmail.com>
2022-09-11 00:37:47 +0300

    add problems from Lesson 6

---
+++ Lesson6/001/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько чисел Вы будете вводить?");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Пожалуйста, введите {arrSize} чисел");
+int[] nums = new int[arrSize];
+
+for (int i = 0; i < arrSize; i++)
+{
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return (count);
+}
+
+Console.WriteLine($"Количество чисел больше 0: {CountPositive(nums)}");
---
+++ Lesson6/002/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Пожалуйста, введите значения для двух прямых.");
+Console.Write("Значение b1:");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Значение k1:");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Значение b2:");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Значение k2:");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+void GetCrossPoint(int b1, int k1, int b2, int k2)
+{
+    // double x = (b1 - b2) / (k2 - k1);
+    double x = ((double)(b1 - b2) / (k2 - k1));
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"Прямые пересекаются в точке с координатами x:{x}, y:{y}");
+}  
+
+GetCrossPoint(b1, k1, b2, k2);
